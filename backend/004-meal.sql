68a4879ceb81ad4b1bbdba258fc9b5a167f71dbb
Shanthi SA <shanthi.shanmugam@gmail.com>
2020-03-16 23:20:25 -0300

    Added meal model and updated readme for env setup

---
+++ backend/004-meal.sql
@@ -0,0 +1,44 @@
+BEGIN;
+create table if not exists app.meal (
+    id bigserial primary key,
+    code text not null,
+    name_en TEXT not NULL,
+    name_fr TEXT,
+    tags text[],
+    description_en text,
+    description_fr text,
+    categories text[],
+    photo_url text,
+    video_url text,
+    method text,
+    cooking_duration numeric,
+    total_cost numeric,
+    serving_cost numeric,
+    tips text,
+    servings_size numeric,
+    serves numeric,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+);
+comment on table app.meal is 'Table to store meal details';
+
+create trigger tg_meal_set_updated_at before update
+on app.meal 
+for each row execute procedure app.set_updated_at();
+COMMIT;
+
+create or replace function app.meal_nutrition(m app.meal) returns setof app.nutrition as $$
+  select * from app.nutrition n where n.nutritionable_id=m.id and n.nutritionable_type='meal';
+$$ language sql stable;
+
+GRANT SELECT, INSERT, UPDATE, DELETE on TABLE app.meal 
+to app_meal_designer, app_admin;
+
+GRANT SELECT on table app.meal 
+to app_anonymous, app_user, app_meal_designer, app_admin;
+
+GRANT USAGE on SEQUENCE app.meal_id_seq 
+to app_meal_designer, app_admin;
+
+GRANT EXECUTE on FUNCTION app.meal_nutrition(app.meal) 
+to app_anonymous, app_user, app_meal_designer, app_admin;
--- backend/db-reset.sh
+++ backend/db-reset.sh
@@ -3,4 +3,5 @@ cat setup-user.sql | envsubst | psql -h db -U postgres -f - && \
 psql -h db -U postgres $DBNAME -f 000-common-utils.sql  && \
 psql -h db -U postgres $DBNAME -f 001-auth-system.sql && \
 psql -h db -U postgres $DBNAME -f 002-product.sql && \
-psql -h db -U postgres $DBNAME -f 003-nutrition.sql 
+psql -h db -U postgres $DBNAME -f 003-nutrition.sql && \
+psql -h db -U postgres $DBNAME -f 004-meal.sql
