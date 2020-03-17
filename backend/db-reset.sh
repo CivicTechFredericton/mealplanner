4b71ce6bd5b86a1ef0a9f732846915488e38957b
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2020-03-17 17:35:06 -0300

    add a single docker compose for both frontend and backend

--- backend/db-reset.sh
+++ backend/db-reset.sh
@@ -1,4 +1,4 @@
-dropdb -h db -U postgres --if-exists $DBNAME && createdb -h db -U postgres $DBNAME
+dropdb --if-exists -h db -U postgres $DBNAME && createdb -h db -U postgres $DBNAME
 cat setup-user.sql | envsubst | psql -h db -U postgres -f - && \
 psql -h db -U postgres $DBNAME -f 000-common-utils.sql  && \
 psql -h db -U postgres $DBNAME -f 001-auth-system.sql && \
