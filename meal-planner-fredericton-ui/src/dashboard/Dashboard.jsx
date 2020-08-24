5225b52b28e7e43e66755870bee4f86dc9ebdec0
albertlockett <albert.lockett@gmail.com>
2020-08-24 19:56:54 -0300

    feat(126): can search for meal by client

--- meal-planner-fredericton-ui/src/dashboard/Dashboard.jsx
+++ meal-planner-fredericton-ui/src/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
 import graphql from 'babel-plugin-relay/macro';
@@ -11,16 +11,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography';
 import Box  from '@material-ui/core/Box';
+import MenuItem from '@material-ui/core/MenuItem';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow'
+import Select from '@material-ui/core/Select'
+import Toolbar from '@material-ui/core/Toolbar'
+import Tooltip from '@material-ui/core/Tooltip'
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList'
 
 import Header from '../core/header/Header';
 import Footer from "../core/footer/Footer";
@@ -88,7 +94,34 @@ const useStyles = makeStyles(theme => ({
         color: theme.palette.text.secondary,
       }
     }
-  }    
+  },   
+  toolbar: {
+    color: 'black',
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    '& > span': {
+      display: 'grid',
+      justifyContent: 'right',
+    }
+  },
+  filterMenu: {
+    position: 'absolute',
+    padding: '16px',
+    bottom: '-70%',
+    right: 0,
+    '& .MuiTypography-root': {
+      color: 'black',
+      display: 'inline-block',
+      marginRight: '10px',
+    },
+    '& .MuiSelect-root': {
+      minWidth: '100px',
+      color: 'black'
+    },
+  },
+  filterLisItem: {
+    color: 'black'
+  }
 }));
 
 function doDeleteMeapPlan({ id }) {
@@ -117,12 +150,52 @@ function doDeleteMeapPlan({ id }) {
 }
 
 const Dashboard = props => {
+  const [filterMenuOpen, setFilterMenuOpen] = useState(false)
+  const [clientsFilter, setClientsFilter] = useState(null)
+
+  const toggleFilterMenuOpen = () => setFilterMenuOpen(!filterMenuOpen)
+
+
   const classes = useStyles();
-  const rows = (props.mealPlans?.nodes ?? []).map(meal => ({
-    ...meal,
-    label: meal.nameEn,
-    createdAt: meal.createdAt
-  }))
+  const rows = (props.mealPlans?.nodes ?? [])
+    .filter(meal => {
+      if (clientsFilter) {
+        return meal.client?.clientId === clientsFilter
+      } else {
+        return true
+      }
+    })
+    .map(meal => ({
+      ...meal,
+      label: meal.nameEn,
+      createdAt: meal.createdAt
+    }))
+
+
+
+  const filterMenu = filterMenuOpen ? (
+    <Paper className={classes.filterMenu}>
+      <Typography align="center">
+        Client ID:
+      </Typography>
+      <Select
+        value={clientsFilter}
+        onChange={e => setClientsFilter(e.target.value)}
+      >
+        {(props.clients?.nodes ?? []).map(client => (
+          <MenuItem
+            className={classes.filterLisItem}
+            value={client.clientId}
+          >
+              {client.clientId}
+          </MenuItem>
+        ))}
+      </Select>
+    </Paper>
+  ) : null
+  
+
+
 
   return (
     <>
@@ -137,34 +210,25 @@ const Dashboard = props => {
         <Grid container
           direction="row"
           justify="space-evenly"
-          alignItems="center" spacing={3}
-                
+          alignItems="center" spacing={3}       
         >
           <Grid item>
-            <Paper className={classes.paper}>
-                   
-            </Paper>
+            <Paper className={classes.paper} />
             <Typography variant="h6" align="center">
               <Link to='/catalogue-meals' className={classes.link}>Catalogue of Meals</Link>
             </Typography>
-                    
           </Grid>
           <Grid item>
             <Paper className={classes.paper}>
-              <ButtonBase className={classes.img}>
-              
-              </ButtonBase>        
+              <ButtonBase className={classes.img} /> 
             </Paper>
             <Typography variant="h6"  align="center">
               <Link to='/catalogue-units' className={classes.link}>Catalogue of Units</Link>
             </Typography>
-                    
           </Grid>
           <Grid item>
             <Paper className={classes.paper}>
-              <ButtonBase className={classes.img}>
-              
-              </ButtonBase>      
+              <ButtonBase className={classes.img} />
             </Paper>
             <Typography variant="h6"  align="center">
               <Link to='/catalogue-products' className={classes.link}>Catalogue of Products</Link>
@@ -172,9 +236,7 @@ const Dashboard = props => {
           </Grid>
           <Grid item>
             <Paper className={classes.paper}>
-              <ButtonBase className={classes.img}>
-              
-              </ButtonBase>      
+              <ButtonBase className={classes.img} />      
             </Paper>
             <Typography variant="h6" align="center">
               <Link to='/clients' className={classes.link}>Clients</Link>
@@ -184,10 +246,24 @@ const Dashboard = props => {
 
         <Box m={8}>
           <TableContainer component={Paper}>
+            <Toolbar className={classes.toolbar}>
+              <Typography variant="h6">Meal Plans</Typography>
+              <span>
+                <span>
+                  <Tooltip title="Filter list" onClick={toggleFilterMenuOpen}>
+                    <IconButton aria-label="filter list">
+                      <FilterListIcon />
+                    </IconButton>
+                  </Tooltip>
+                </span>
+                {filterMenu}
+              </span>
+            </Toolbar>
             <Table color="primary" className={classes.table} aria-label="simple table">
               <TableHead>
                 <TableRow>
                   <TableCell>Meal Plans Created</TableCell>
+                  <TableCell>Client</TableCell>
                   <TableCell align="center">Date Created</TableCell>
                   <TableCell align="center">Edit</TableCell>
                   {/* <TableCell align="center">Clone</TableCell> */}
@@ -208,6 +284,9 @@ const Dashboard = props => {
                       >
                         {row.label}
                       </Link>
+                    </TableCell>
+                    <TableCell>
+                      {row.client?.clientId ?? 'Unassigned'}
                     </TableCell>
                     <TableCell align="right">
                       {row.createdAt}
@@ -266,6 +345,14 @@ query DashboardQuery	 {
       id
       nameEn
       createdAt
+      client {
+        clientId
+      }
+    }
+  }
+  clients {
+    nodes {
+      clientId
     }
   }
 }
---
+++ meal-planner-fredericton-ui/src/planpage/MealPlanAssignment.jsx
@@ -0,0 +1,145 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import CancelIcon from '@material-ui/icons/Cancel'
+import EditIcon from '@material-ui/icons/Edit'
+import CheckIcon from '@material-ui/icons/Check'
+import SaveIcon from '@material-ui/icons/Save'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { Modal } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  autocompleteText: {
+    color: 'black',
+    '& .MuiInputBase-input': {
+      color: 'black',
+    }
+  },
+  button: {
+    color: 'black',
+    minWidth: 0,
+    padding: 0,
+    fontSize: '8px',
+    '& .MuiButton-label': {
+      color: 'black',
+    }
+  },
+  paper: {
+    position: 'absolute',
+    height: 180,
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '100px',
+    left: 'calc(50% - 200px)',
+  },
+}))
+
+export default function MealPlanAssignment(props) {
+  const classes = useStyles()
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalValue, setModalValue] = useState(null)
+
+  const options = props.allClients?.nodes ?? []
+
+  useEffect(() => {
+    if (props.selectedPlan?.clientId) {
+      const option = options.find(({ rowId }) => rowId === props.selectedPlan?.clientId)
+      if (option) {
+        setModalValue(option)
+        props.setAssignedClientId(option)
+      }
+    }
+  }, [
+    props.selectedPlan,
+    props.allClients
+  ])
+
+  if (!props.selectedPlan) {
+    // no point in rendering anything, cannot show assignment if no meal
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <Typography>
+        <b>Client Assignment:</b>
+        <Button className={classes.button} onClick={() => setModalOpen(true)}>
+          <EditIcon fontSize={'8px'} />
+        </Button>
+      </Typography>
+      <Typography>
+        {props.assignedClientId ? props.assignedClientId.clientId : 'This meal plan is unassigned'}
+      </Typography>
+      <Modal open={modalOpen}>
+        <div className={classes.paper}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography variant="h4">Change Client Assignment</Typography>
+              <br />
+            </Grid>
+            <Grid item xs={12}>
+              <Autocomplete
+                id="combo-box-demo"
+                options={options}
+                getOptionLabel={(option) => option.clientId}
+                style={{ width: 300 }}
+                renderInput={(params) => 
+                  <TextField 
+                    className={classes.autocompleteText}
+                    {...params} 
+                    variant="outlined"
+                  />
+                }
+                renderOption={params =>{
+                  return <span style={{color: 'black'}}>{params.clientId}</span>
+                }}
+                value={modalValue}
+                onChange={(event, val) => {
+                  console.log({ val })
+                  setModalValue(val)
+                }}
+              />
+              <br />
+              <br />
+            </Grid>
+            <Grid container item justify="flex-end" >
+              <Button
+                color="primary"
+                onClick={() => {
+                  props.setAssignedClientId(modalValue)
+                  setModalOpen(false)
+                }}
+              >
+                OK <CheckIcon />
+              </Button>
+              <Button
+                color="secondary" 
+                onClick={() => {
+                  setModalOpen(false)
+                }}
+              >
+                Cancel
+                <CancelIcon />
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+
+}
+
+MealPlanAssignment.propTypes = {
+  assignedClientId: PropTypes.string,
+  setAssignedClientId: PropTypes.func,
+  selectedPlan: PropTypes.object,
+}
