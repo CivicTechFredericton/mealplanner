833082ec3fe9d932d6c24d0488c69c6c901eee00
Rory <rorybray@users.noreply.github.com>
2020-12-06 01:05:35 -0400

    backend is restartable db-reset is only done on first run or when .reset-db is touched

--- backend/start_postgraphile.sh
+++ backend/start_postgraphile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-sleep 10
-
-PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -h db  -f db-reset.psql
+# sleep in case this is the first build, postgres takes time to init
+sleep 5
+# test for the presence of the schema
+PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -h db  -c "select count(*) from app_private.account"
+if [ $? != 0 ] || [ -f .reset-db ]; then
+	PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -h db  -f db-reset.psql
+fi
+rm -f .reset-db
 npm start
