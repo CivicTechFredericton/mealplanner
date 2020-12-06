63edd57f2b6a9672250a017d7928ad6de181677a
Rory <rorybray@users.noreply.github.com>
2020-12-06 01:31:32 -0400

    Added migrations to the startup and eliminated the non-Windows db-reset

--- backend/start_postgraphile.sh
+++ backend/start_postgraphile.sh
@@ -2,9 +2,16 @@
 # sleep in case this is the first build, postgres takes time to init
 sleep 5
 # test for the presence of the schema
-PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -h db  -c "select count(*) from app_private.account"
+export PGPASSWORD=${POSTGRES_PASSWORD}
+
+psql -U postgres -h db  -c "select count(*) from app_private.account"
 if [ $? != 0 ] || [ -f .reset-db ]; then
-	PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -h db  -f db-reset.psql
+	psql -U postgres -h db -f db-reset.psql
 fi
 rm -f .reset-db
+# run through the migrations
+for sql in migrations/*.sql
+do
+	psql -U postgres -h db  -f "${sql}"
+done
 npm start
