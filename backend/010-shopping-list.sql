7dc55c4151853e732a6061c0b8d5e5b11c5d8cda
Rory <rorybray@users.noreply.github.com>
2021-01-31 14:36:28 -0400

    Updated shopping list generation to be per meal_plan instead of per person
    removed meal_plan_id from person

--- backend/010-shopping-list.sql
+++ backend/010-shopping-list.sql
@@ -1,13 +1,8 @@
 BEGIN;
 
--- alter the table person to add meal plan id as a foreign key
--- create index for the above
 -- write a function that makes a query to join meal plan with meals with products and measure and 
 -- returns product id, product name, quantity, unit
-
-ALTER TABLE app.person ADD meal_plan_id BIGINT references app.meal_plan(id);
-
-create index idx_person_meal_plan_id on app.person(meal_plan_id);
+-- produces a shopping list for a single meal plan
 
 create type app.shopping_list_item as (
     product_id BIGINT,
@@ -16,17 +11,17 @@ create type app.shopping_list_item as (
     unit text
 );
 
-create or replace function app.person_shopping_list(p app.person) returns setof app.shopping_list_item as $$
+create or replace function app.meal_plan_shopping_list(p app.meal_plan) returns setof app.shopping_list_item as $$
 
 select app.product.id, app.product.name_en, SUM(app.measure.quantity), app.measure.unit 
-from app.person
-JOIN app.meal_plan_entry on app.person.meal_plan_id=app.meal_plan_entry.meal_plan_id
+from app.meal_plan
+JOIN app.meal_plan_entry on app.meal_plan.id=app.meal_plan_entry.meal_plan_id
 JOIN app.measure on app.meal_plan_entry.meal_id=app.measure.meal_id
 JOIN app.product on app.measure.product_id=app.product.id
-WHERE app.person.id = p.id
+WHERE app.meal_plan.id = p.id
 GROUP BY app.product.id, app.product.name_en, app.measure.unit;
 
 $$ language sql stable;
 
-GRANT execute on function app.person_shopping_list(app.person) to app_user, app_meal_designer, app_admin;
+GRANT execute on function app.meal_plan_shopping_list(app.meal_plan) to app_user, app_meal_designer, app_admin;
 COMMIT;
