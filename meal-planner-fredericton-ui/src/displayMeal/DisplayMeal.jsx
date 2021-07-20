9d22072872e7067f5c1c62d2fcc8d2716a328278
Colton Patterson <colton.patterson@outlook.com>
2021-07-20 18:00:40 -0300

    fixed DisplayMeal.jsx

--- meal-planner-fredericton-ui/src/displayMeal/DisplayMeal.jsx
+++ meal-planner-fredericton-ui/src/displayMeal/DisplayMeal.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import graphql from 'babel-plugin-relay/macro';
 import Grid from '@material-ui/core/Grid';
 //import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import Image from 'material-ui-image';
+import AddIcon from '@material-ui/icons/Add';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Header from '../core/header/Header';
 import Footer from '../core/footer/Footer';
@@ -10,6 +13,7 @@ import { useParams } from "react-router-dom";
 import { QueryRenderer} from 'react-relay';
 import environment from '../relay-environment';
 import StarRating from '../components/starRating/StarRating';
+import IframeResizer from 'iframe-resizer-react';
 
 // const useStyles = makeStyles(theme => ({
 //   root: {
@@ -40,9 +44,9 @@ import StarRating from '../components/starRating/StarRating';
 //   },
 // }));
 
+
 const DisplayMeal = (props) => {
   let { id } = useParams();
-  console.log(id);
   return (
     <QueryRenderer
       environment={environment}
@@ -94,68 +98,70 @@ const renderQuery = ({error, props}) => {
   } else if (props) return (
     <div>
       <Header />
-      <Grid container component="main" style={{paddingTop: 75, fontFamily: 'Open Sans'}} xs={12}>
-        <Grid item xs={12}>
-          <Typography variant="body" color="textSecondary" align="center">
-            <h1>{props.mealById.nameEn}</h1>
-          </Typography>
+      <Grid container component="main" style={{fontFamily: 'Open Sans'}} xs={12}>
+        <Grid justify="center" item xs={12}>    
+            <Image cover={true} src={props.mealById.photoUrl}/>
         </Grid>
-        <Grid container item xs={12}>
-          <Grid item xs={8}>
-            <Typography align="center">
-              <img src={props.mealById.photoUrl} width="400" height="400"/>
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Box mt={10} ml={3} mr={5}>
-              <h2>Nutrition Score: <StarRating halfStars={props.mealById.nutritionRating}></StarRating></h2>
-              <h2 style={{whiteSpace: 'pre-wrap'}}>
-                {props.mealById.descriptionEn}{"\n"}
-                            Ready in: {props.mealById.cookingDuration} minutes{"\n"}
-                            Serves: {props.mealById.serves}
-              </h2>
+        <Grid xs={12} component="body" color="textSecondary" >
+          <Box paddingLeft="2%" paddingRight="2%">
+            <div style={{fontSize: 60 }}><ArrowBackIosIcon fontSize="large"/>{props.mealById.nameEn}
+            &nbsp;
+            <Button style={{backgroundColor: '#6aa64a', color: 'white', float: 'right', marginTop: '2em'}} variant="contained"><AddIcon/>Add Meal</Button>
+            </div>  
+          </Box>                                                                                                     
+        </Grid>
+        <Grid container xs={12}>
+          <Grid xs={6}>
+            <Box mt={2} ml={10} mr={4}>
+              <IframeResizer width="100%" height={400}
+                src={props.mealById.videoUrl} 
+                frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+                              picture-in-picture; fullscreen" allowFullScreen>
+              </IframeResizer>
             </Box>
           </Grid>
-        </Grid>
+          <Grid xs={6}>
+            <Box mt={2} mr={10}>
+              <p>{props.mealById.descriptionEn}</p>
+            </Box>
+          </Grid> 
+        </Grid>  
         <Grid container item xs={12}>
-          <Grid item xs={6} m={10}>
-            <Box m={10}>
-              <h2>Ingredients: </h2>
+          <Box mt={2} ml={10} mr={10}>
+            <div>
+              <p><b>Ready in:</b>&nbsp;{props.mealById.cookingDuration} minutes<br/></p>
+              <p><b>Serves:</b>&nbsp; {props.mealById.serves}</p>
+            </div>  
+          </Box>  
+        </Grid>
+        <Grid container xs={12}>
+          <Grid item xs={6}>
+            <Box mt={2} ml={10} mr={10}>
+              <h1>Ingredients</h1>
               <p style={{fontSize: 20}}>
                 <dl>
                   {props.mealById.measures.nodes.map(measure => (
-			  <div>
-                    <dt key={measure} style={{float: 'left', width: '15%', textAlign: 'right', paddingRight: '2%'}}>
+			          <div>
+                    <p>
+                      {measure.product.nameEn}&nbsp;-&nbsp;               
                       {measure.quantity}&nbsp;
-                      {measure.unit}&nbsp;
-                    </dt>
-                    <dd>
-                      {measure.product.nameEn}
-                    </dd>
-			  </div>
+                      {measure.unit}&nbsp;                    
+                    </p>             
+			          </div>
                   ))}
                 </dl>
               </p>
             </Box>
           </Grid>
           <Grid item xs={6}>
-            <Box m={10}>
-              <h2>Direction: </h2>
-              <p style={{fontSize: 20, whiteSpace: 'pre-wrap'}}>
+            <Box mt={2} mr={10}>
+              <h1>Directions</h1>
+              <p style={{fontSize: 20}}>
                 {props.mealById.method}
               </p>
             </Box>
           </Grid>
         </Grid>
-        <Grid container xs={12}>
-          <Box ml ={10} mr={10}>
-            <iframe width="840" height="480" 
-              src={props.mealById.videoUrl} 
-              frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
-                            picture-in-picture; fullscreen" allowFullScreen>
-            </iframe>
-          </Box>
-        </Grid>  
       </Grid>
       <Grid style={{marginTop: 100}}>
         <Footer />
