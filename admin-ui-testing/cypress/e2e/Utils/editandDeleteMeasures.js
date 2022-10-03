a1e440cab88cc0794bfee1656f3f23cdeab0a5e8
SadPlum <sebastienolegault@gmail.com>
2022-10-03 20:46:45 -0300

    Fixed measures

--- admin-ui-testing/cypress/e2e/Utils/editandDeleteMeasures.js
+++ admin-ui-testing/cypress/e2e/Utils/editandDeleteMeasures.js
@@ -1,32 +1,39 @@
-import editandDeleteMeasuresPage from "../Pages/editandDeleteMeasuresPage.js"
+import editandDeleteMeasuresPage from '../Pages/editandDeleteMeasuresPage.js';
 const editandDeleteMeasures = new editandDeleteMeasuresPage();
 
+// Changes Measures quantities
 Cypress.Commands.add('EditMeasure', (data) => {
+  editandDeleteMeasures
+    .InputmeasuringQunatity()
+    .clear()
+    .type(data.editQuantity);
+  editandDeleteMeasures.InputmeasuringUnit().clear().type(data.editUnit);
+  editandDeleteMeasures.clickSaveBtn().click();
+  cy.contains('Element updated').should('be.visible');
+});
 
-    editandDeleteMeasures.clickMeasuresonMenu().click()
-
-    cy.get("table tbody tr").wait(1000).contains('tr', 'Green Bell Pepper').within(() => {
-
-        //click 'Edit' of Measures
-        cy.contains("Edit").click({ force: true });
-        cy.url().should('include', "/measures/")
-        cy.wait(1000)
-        editandDeleteMeasures.InputmeasuringQunatity().clear().type(data.editQuantity)
-        editandDeleteMeasures.InputmeasuringUnit().clear().type(data.editUnit)
-        editandDeleteMeasures.clickSaveBtn().click()
-        cy.contains("Element updated").should('be.visible')
-
-    })
+// Opens the Measures Edit
+Cypress.Commands.add('OpenMeasure', () => {
+  editandDeleteMeasures.clickMeasuresonMenu().click();
+  cy.get('table tbody tr')
+    .wait(1000)
+    .contains('tr', 'Green Bell Pepper')
+    .within(() => {
+      //click 'Edit' of Measures
+      cy.contains('Edit').click({ force: true });
+      cy.url().should('include', '/measures/');
+      cy.wait(1000);
+    });
 });
 
 Cypress.Commands.add('DeleteMeasure', (data) => {
+  editandDeleteMeasures.clickMeasuresonMenu().click();
 
-    editandDeleteMeasures.clickMeasuresonMenu().click()
-
-    cy.get("table tbody tr").wait(1000).contains('tr', 'Green Bell Pepper').within(() => {
-
-        //Delete the measures
-        editandDeleteMeasures.selectcheckbox().click({ force: true })
-
-    })
-})
+  cy.get('table tbody tr')
+    .wait(1000)
+    .contains('tr', 'Green Bell Pepper')
+    .within(() => {
+      //Delete the measures
+      editandDeleteMeasures.selectcheckbox().click({ force: true });
+    });
+});
--- admin-ui-testing/cypress/e2e/editandDeleteMeasures.cy.js
+++ admin-ui-testing/cypress/e2e/editandDeleteMeasures.cy.js
@@ -1,23 +1,21 @@
-import testdata from "../fixtures/testdata.json";
-import editMeasurestestdata from "../fixtures/editMeasurestestdata.json"
+import testdata from '../fixtures/testdata.json';
+import editMeasurestestdata from '../fixtures/editMeasurestestdata.json';
 
 describe('testEditandDeleteMeasuresofaProductforMeal', () => {
+  beforeEach(function () {
+    cy.login(testdata);
+  });
 
-    beforeEach(function () {
-        cy.login(testdata);
-    });
+  it('edittheproductdetails', function () {
+    cy.OpenMeasure();
+    cy.EditMeasure(editMeasurestestdata);
+  });
 
-    it('edittheproductdetails', function () {
-        cy.EditMeasure(editMeasurestestdata)
+  it('deletetheproductdetails', function () {
+    cy.DeleteMeasure();
+  });
 
-    });
-
-    it('deletetheproductdetails', function() {
-        cy.DeleteMeasure()
-    })
-
-    it('Validate successful Logout', function () {
-        cy.logout()
-    })
-    
-})
+  //   it('Validate successful Logout', function () {
+  //     cy.logout();
+  //   });
+});
