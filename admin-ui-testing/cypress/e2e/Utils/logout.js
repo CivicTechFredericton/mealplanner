8c689d2f6d03a72cb8fca98dfc2af8d1e142e90a
SadPlum <sebastienolegault@gmail.com>
2022-10-10 17:32:01 -0300

    fixed logoutpage bug and added dependencies

--- admin-ui-testing/cypress/e2e/Utils/logout.js
+++ admin-ui-testing/cypress/e2e/Utils/logout.js
@@ -1,12 +1,11 @@
-import loginPage from '../Pages/loginPage.js';
-const login = new loginPage();
-import logoutPage from '../pages/logoutPage.js'
-const logout = new logoutPage();
+import loginPage from '../Pages/loginPage.js'
+const login = new loginPage()
+import logoutPage from '../Pages/logoutPage.js'
+const logout = new logoutPage()
 
 Cypress.Commands.add('logout', () => {
-
-    logout.clickAccountCircleIcon().click({force:true})
-    cy.wait(1000)
-    logout.clicklogout().click({force:true})
-    login.usernameInput().should('be.visible')
+  logout.clickAccountCircleIcon().click({ force: true })
+  cy.wait(1000)
+  logout.clicklogout().click({ force: true })
+  login.usernameInput().should('be.visible')
 })
