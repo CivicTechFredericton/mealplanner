9eb262b150dd209a02585e43cc59c2469e1dc76b
Rory <rorybray@users.noreply.github.com>
2023-06-02 19:50:01 -0300

    kubernetes deployment initial checkin

--- backend/start_postgraphile.sh
+++ backend/start_postgraphile.sh
@@ -5,14 +5,14 @@ export PGPASSWORD=${POSTGRES_PASSWORD}
 
 sleep 5
 
-psql -U postgres -h db  -c "select count(*) from app_private.account"
+psql -U postgres -h $PG_HOST  -c "select count(*) from app_private.account"
 if [ $? != 0 ] || [ -f .reset-db ]; then
-	psql -U postgres -h db -f db-reset.psql
+	psql -U postgres -h $PG_HOST -f db-reset.psql
 fi
 rm -f .reset-db
 # run through the migrations
 for sql in migrations/*.sql
 do
-	psql -U postgres -h db  -f "${sql}"
+	psql -U postgres -h $PG_HOST  -f "${sql}"
 done
 node server.js
