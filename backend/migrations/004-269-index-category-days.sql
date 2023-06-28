13f10192d5b515cb9d11b98ad3bd88280bbe4c90
Rory <rorybray@users.noreply.github.com>
2023-06-28 20:15:26 -0300

    #467 added 'if exists' to supress errors

--- backend/migrations/004-269-index-category-days.sql
+++ backend/migrations/004-269-index-category-days.sql
@@ -1,2 +1,2 @@
-create index idx_meal_plan_entry_category on app.meal_plan_entry(category);
-create index idx_meal_plan_entry_days on app.meal_plan_entry(days);
+create index if not exists idx_meal_plan_entry_category on app.meal_plan_entry(category);
+create index if not exists idx_meal_plan_entry_days on app.meal_plan_entry(days);
