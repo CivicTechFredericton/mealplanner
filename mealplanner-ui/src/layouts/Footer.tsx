4fe278f6018dd7979ca24e6bef96f9376a859745
Pujitha <dpujitha.sdm@gmail.com>
2023-07-12 11:55:01 -0300

    [469]Added Logo and made changes to the footer

--- mealplanner-ui/src/layouts/Footer.tsx
+++ mealplanner-ui/src/layouts/Footer.tsx
@@ -1,5 +1,6 @@
 //Reference: https://github.com/mui/material-ui/blob/master/docs/data/material/getting-started/templates/sticky-footer/StickyFooter.tsx
 import { Box, Container, Typography } from "@mui/material";
+import Logo from "../CivicTechLogo.png";
 
 export function Footer() {
   return (
@@ -16,9 +17,12 @@ export function Footer() {
       }}
     >
       <Container maxWidth="xl">
-        <Typography variant="body1" align="center">
-          For Greener Village. By Civic Tech Fredericton.
-        </Typography>
+      <Box display= 'flex' alignItems= 'center' justifyContent='center'>
+          <Typography variant="body1" align="center">
+            For Greener Village. Developed by
+          </Typography>
+          <img src={Logo} alt="Logo" style={{width: "10%", height: "auto", marginLeft: 1}} /> 
+        </Box>
         <Typography variant="body2" align="center">
           If you run into issues or have any suggestions or questions, please
           feel free to post your{" "}
