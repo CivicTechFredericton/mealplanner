b24686e82a35d61531abc1282c6e31d21e14cfb6
Pavithra Sunkesulu <pavi.kec@gmail.com>
2023-10-03 11:42:59 -0300

    Added unit conversion table for Products.

--- admin-ui/src/Products/ProductList.tsx
+++ admin-ui/src/Products/ProductList.tsx
@@ -8,12 +8,44 @@ import {
   ReferenceManyField,
   SingleFieldList,
   TextField,
+  FunctionField,
   UrlField,
 } from "react-admin";
 import { ListField } from "../ListField";
 import { NutritionShow } from "../Nutrition/NutritionShow";
 
 export const ProductList = (props: ListProps) => {
+  const convertToKilograms = (value: number, unit: string) => {
+    if (unit === "g") {
+      return (value / 1000).toFixed(2);
+    } else if (unit === "lb") {
+      return (value / 2.2).toFixed(2);
+    } else {
+      return value;
+    }
+  };
+
+  const convertToLiters = (value: number, unit: string) => {
+    if (unit === "ml" || unit === "mL") {
+      return value / 1000;
+    } else if (unit === "oz") {
+      return value * 0.0295735;
+    } else {
+      return value;
+    }
+  };
+
+  const formatUnit = (unit: string) => {
+    const unitMapping: Record<string, string> = {
+      g: "kg",
+      lb: "kg",
+      ml: "L",
+      mL: "L",
+      oz: "L",
+    };
+    return unitMapping[unit] || unit;
+  };
+
   return (
     <List {...props} title="ProductList">
       <Datagrid expand={NutritionDetails}>
@@ -22,8 +54,23 @@ export const ProductList = (props: ListProps) => {
         <TextField source="nameFr" fullWidth />
         <TextField source="code" />
         <NumberField source="price" />
-        <NumberField source="quantity" />
-        <TextField source="unit" />
+        <FunctionField
+          label="Quantity"
+          render={(record: any) => {
+            const convertedQuantity =
+              record.unit === "g" || record.unit === "lb"
+                ? convertToKilograms(record.quantity, record.unit)
+                : convertToLiters(record.quantity, record.unit);
+
+            return `${convertedQuantity}`;
+          }}
+        />
+        <FunctionField
+          label="Unit"
+          render={(record: any) => {
+            return formatUnit(record.unit);
+          }}
+        />
         <BooleanField source="isArchived" />
         <TextField source="upc" />
         <UrlField source="sourceLink" />
