d95bffd08c3dafa4be7e37665c23466832e7effd
Rodrigo Castro <castro.rl@gmail.com>
2024-02-05 10:27:46 -0400

    removing unecessary file

---
+++ admin-ui/src/Measure/service.ts
@@ -0,0 +1,40 @@
+import { ApolloClient, gql } from '@apollo/client';
+
+const searchStringQuery = gql`
+  query SearchStrings($searchString: String!) {
+    query {
+      meals(
+        filter: {
+          or: [
+            { code: { includes: $searchString } }
+            { nameEn: { includes: $searchString } }
+            { nameFr: { includes: $searchString } }
+            { descriptionEn: { includes: $searchString } }
+            { descriptionFr: { includes: $searchString } }
+          ]
+        }
+      ) {
+        edges {
+          node {
+            rowId
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getSearchByString = async (
+  client: ApolloClient<object>,
+  searchString: string
+): Promise<any> => {
+  let result = await client.query({
+    query: searchStringQuery,
+    variables: { searchString },
+  });
+  return result;
+};
+
+export type MealType = {
+  rowId: string;
+};
