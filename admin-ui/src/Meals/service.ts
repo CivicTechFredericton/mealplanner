4ab0094bae54475f40386c4104dd031eda35442c
Rodrigo Castro <castro.rl@gmail.com>
2024-02-07 14:27:07 -0400

    Added a new query for Category, and added a verification to execute one the string query or the category query depending on the input string

--- admin-ui/src/Meals/service.ts
+++ admin-ui/src/Meals/service.ts
@@ -25,22 +25,52 @@ const searchStringQuery = gql`
   }
 `;
 
+const searchStringCategoryQuery = gql`
+  query searchCategory($searchString: CategoryT!) {
+    query {
+      meals(filter: { categories: { anyEqualTo: $searchString } }) {
+        edges {
+          node {
+            rowId
+          }
+        }
+      }
+    }
+  }
+`;
+
+const extractIdsFromResult = (result: any) => {
+  const meals = result?.data?.query?.meals?.edges || [];
+  type EdgeType = { node: MealType };
+  const extractedMeals: MealType[] = meals.map((edge: EdgeType) => edge.node);
+  return extractedMeals.map((id) => id.rowId);
+};
+
 export const getSearchByString = async (
   client: ApolloClient<object>,
   searchString: string
 ): Promise<any> => {
-  return await client.query({
-    query: searchStringQuery,
-    variables: { searchString },
-  }).then(result => {
-    const meals = result?.data?.query?.meals?.edges || [];
-    type EdgeType = { node: MealType };
-    const extractedMeals: MealType[] = meals.map((edge: EdgeType) => edge.node);
-    const idsArray = extractedMeals.map((id) => id.rowId);
-    return idsArray; // Return only the needed ids
-  });
+  const isCategory = ['BREAKFAST', 'LUNCH', 'DINNER', 'SNACK'].includes(searchString.toUpperCase());
+
+  if (isCategory) {
+    return await client
+      .query({
+        query: searchStringCategoryQuery,
+        variables: { searchString },
+      })
+      .then((result) => extractIdsFromResult(result));
+  }
+  return await client
+    .query({
+      query: searchStringQuery,
+      variables: { searchString },
+    })
+    .then((result) => extractIdsFromResult(result));
 };
+
 
 export type MealType = {
   rowId: string;
 };
+
+export type CategoryT = 'BREAKFAST' | 'LUNCH' | 'DINNER' | 'SNACK' | null;
