0d6fa4f807ea9f0b092e9f417d23d2da372480ff
Rodrigo Castro <castro.rl@gmail.com>
2024-02-09 17:22:49 -0400

    Created the service.ts with the graphql queries, the functions to fetch data from the database and return an array of ids

---
+++ admin-ui/src/Products/service.ts
@@ -0,0 +1,49 @@
+import { ApolloClient, gql } from '@apollo/client';
+
+const searchStringQuery = gql`
+  query SearchStrings($searchString: String!) {
+    query {
+      products(
+        filter: {
+          or: [
+            { code: { includes: $searchString } }
+            { nameEn: { includes: $searchString } }
+            { nameFr: { includes: $searchString } }
+            { upc: { includes: $searchString } }
+            { tags: { contains: [$searchString] } }
+          ]
+        }
+      ) {
+        edges {
+          node {
+            rowId
+          }
+        }
+      }
+    }
+  }
+`;
+
+const extractIdsFromResult = (result: any) => {
+  console.log(result);
+  const products = result?.data?.query?.products?.edges || [];
+  type EdgeType = { node: ProductType };
+  const extractedProducts: ProductType[] = products.map((edge: EdgeType) => edge.node);
+  return extractedProducts.map((id) => id.rowId);
+};
+
+export const getSearchByString = async (
+  client: ApolloClient<object>,
+  searchString: string
+): Promise<any> => {
+  return await client
+    .query({
+      query: searchStringQuery,
+      variables: { searchString },
+    })
+    .then((result) => extractIdsFromResult(result));
+};
+
+export type ProductType = {
+  rowId: string;
+};
