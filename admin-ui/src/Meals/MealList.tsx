0513d64cb3f8470cb262f04704e35fb51f4bc787
Rodrigo Castro <castro.rl@gmail.com>
2024-02-13 13:57:10 -0400

    changed my Prettier settings to use double quotes instead of single quotes in jsx and tsx files. Updated the entire code to use double quotes

--- admin-ui/src/Meals/MealList.tsx
+++ admin-ui/src/Meals/MealList.tsx
@@ -13,12 +13,12 @@ import {
   TabbedShowLayout,
   TextField,
   UrlField,
-} from 'react-admin';
-import { ListField } from '../ListField';
-import { NutritionShow } from '../Nutrition/NutritionShow';
-import { useState } from 'react';
-import { MealType } from './service';
-import CustomSearchInput from '../components/CustomSearchInput';
+} from "react-admin";
+import { ListField } from "../ListField";
+import { NutritionShow } from "../Nutrition/NutritionShow";
+import { useState } from "react";
+import { MealType } from "./service";
+import CustomSearchInput from "../components/CustomSearchInput";
 
 export const MealList = (props: ListProps) => {
   const [data, setData] = useState<MealType[]>([]);
@@ -31,24 +31,24 @@ export const MealList = (props: ListProps) => {
       <CustomSearchInput onSearch={handleSearchResult} />
       <List
         {...props}
-        title='Meals List'
+        title="Meals List"
         filter={{
           rowId: data,
-          order: 'ASC',
+          order: "ASC",
           page: 1,
           perPage: 10,
-          sort: 'id',
+          sort: "id",
         }}
       >
         <Datagrid expand={Details}>
-          <TextField source='id' />
-          <TextField source='code' />
-          <TextField source='nameEn' />
-          <TextField source='nameFr' />
-          <ListField label='Tags' source='tags' />
-          <TextField source='descriptionEn' />
-          <TextField source='descriptionFr' />
-          <ListField source='categories' />
+          <TextField source="id" />
+          <TextField source="code" />
+          <TextField source="nameEn" />
+          <TextField source="nameFr" />
+          <ListField label="Tags" source="tags" />
+          <TextField source="descriptionEn" />
+          <TextField source="descriptionFr" />
+          <ListField source="categories" />
           <EditButton />
         </Datagrid>
       </List>
@@ -60,40 +60,40 @@ const Details = () => {
   return (
     <>
       <TabbedShowLayout syncWithLocation={false}>
-        <Tab label='Method'>
-          <RichTextField source='method' />
-          <ReferenceManyField label='Measures' reference='measures' target='mealId'>
+        <Tab label="Method">
+          <RichTextField source="method" />
+          <ReferenceManyField label="Measures" reference="measures" target="mealId">
             <Datagrid>
               {/* <TextField source="rowId" /> */}
-              <TextField source='productId' label='Product ID' />
-              <ReferenceField label='Product Name' reference='products' source='productId'>
-                <TextField source='nameEn' />
+              <TextField source="productId" label="Product ID" />
+              <ReferenceField label="Product Name" reference="products" source="productId">
+                <TextField source="nameEn" />
               </ReferenceField>
-              <TextField label='Nom du Produit' source='nameFr' />
-              <TextField source='unit' />
-              <TextField source='quantity' />
+              <TextField label="Nom du Produit" source="nameFr" />
+              <TextField source="unit" />
+              <TextField source="quantity" />
             </Datagrid>
           </ReferenceManyField>
         </Tab>
-        <Tab label='Summary'>
-          <NumberField source='cookingDuration' />
-          <NumberField source='totalCost' />
-          <NumberField source='servingCost' />
-          <TextField source='tips' />
-          <NumberField source='servingsSize' />
-          <TextField source='servingsSizeUnit' />
-          <NumberField source='serves' />
-          <NumberField source='nutritionRating' />
-          <UrlField source='photoUrl' />
-          <UrlField source='videoUrl' />
-          <DateField source='createdAt' showTime />
-          <DateField source='updatedAt' showTime />
+        <Tab label="Summary">
+          <NumberField source="cookingDuration" />
+          <NumberField source="totalCost" />
+          <NumberField source="servingCost" />
+          <TextField source="tips" />
+          <NumberField source="servingsSize" />
+          <TextField source="servingsSizeUnit" />
+          <NumberField source="serves" />
+          <NumberField source="nutritionRating" />
+          <UrlField source="photoUrl" />
+          <UrlField source="videoUrl" />
+          <DateField source="createdAt" showTime />
+          <DateField source="updatedAt" showTime />
         </Tab>
-        <Tab label='Nutrition'>
+        <Tab label="Nutrition">
           <ReferenceManyField
-            reference='nutrition'
-            target='nutritionableId'
-            filter={{ nutritionableType: 'meal' }}
+            reference="nutrition"
+            target="nutritionableId"
+            filter={{ nutritionableType: "meal" }}
           >
             <SingleFieldList>
               <NutritionShow />
