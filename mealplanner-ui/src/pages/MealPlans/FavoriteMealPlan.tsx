a234ed7b5b5193ad47d6fe740a2dd7086692388b
Priya <guntupallisaipriya@gmail.com>
2024-02-20 11:34:04 -0400

    updated sql file

--- mealplanner-ui/src/pages/MealPlans/FavoriteMealPlan.tsx
+++ mealplanner-ui/src/pages/MealPlans/FavoriteMealPlan.tsx
@@ -2,15 +2,29 @@ import { graphql } from "babel-plugin-relay/macro";
 import { commitMutation } from "relay-runtime";
 import environment from "../../relay/environment";
 
-const favoriteMealPlanQuery = graphql`
-query FavoriteMealPlanQuery {
-  favoriteMealPlan {
-    meal_id
-    user_id
-  }
-}
-`;
+// const createFavoriteMealMutation = graphql`
+// mutation CreateFavoriteMealMutation($connections: [ID!]!, $mealId: BigInt!) {
+//   createFavoriteMeal(input: { mealId: $mealId }) {
+//     favoriteMeal {
+//       personId
+//       mealId
+//     }
+//   }
+// }
+// `;
 
+const favoriteMealPlanQuery = graphql`
+  query FavoriteMealsQuery {
+      favoriteMeals(orderBy: [CREATED_AT_DESC], first: 1000) {
+        nodes {
+            id
+            rowId
+            mealId
+            userId
+      }
+      }
+  }
+`;
 
 
 export const favoriteMealPlan = () => {
