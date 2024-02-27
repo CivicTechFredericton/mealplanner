ff16db51a6f7eb081ef8f931fe064ff16a909a8e
okayyjen <bkim3@unb.ca>
2024-02-27 00:40:06 -0400

    Fixes #482 - admin app measure form now has autocomplete feature for Products and Meals fields

--- admin-ui/src/Measure/MeasureForm.tsx
+++ admin-ui/src/Measure/MeasureForm.tsx
@@ -1,27 +1,76 @@
 import {
+  AutocompleteInput,
   NumberInput,
-  ReferenceInput,
   required,
-  SelectInput,
   SimpleForm,
   TextInput,
 } from "react-admin";
+import { gql, useQuery } from "@apollo/client";
+
+const getProductsAndMealsQuery = gql`
+  query GetProductsAndMeals {
+    query {
+      meals {
+        nodes {
+          nameEn
+          rowId
+        }
+      }
+      products {
+        nodes {
+          nameEn
+          rowId
+        }
+      }
+    }
+  }
+`;
+
+interface Product {
+  rowId: string;
+  nameEn: string;
+}
+
+interface Meal {
+  rowId: string;
+  nameEn: string;
+}
 
 export const MeasureForm = () => {
+  const { loading, error, data } = useQuery(getProductsAndMealsQuery);
+  const meals: Meal[] = data?.query?.meals?.nodes;
+  const products: Product[] = data?.query?.products?.nodes;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const mealChoices = meals.map((meal: Meal) => ({
+    id: meal.rowId,
+    nameEn: meal.nameEn,
+  }));
+
+  const productChoices = products.map((product: Product) => ({
+    id: product.rowId,
+    nameEn: product.nameEn,
+  }));
+
   return (
     <SimpleForm>
       <NumberInput source="quantity" />
       <TextInput source="unit" />
-      <ReferenceInput source="productId" reference="products">
-        <SelectInput
+      <AutocompleteInput
           optionText="nameEn"
+          source="productId"
           label="Product"
           validate={[required()]}
+          choices={ productChoices }
+      />
+      <AutocompleteInput
+          optionText="nameEn"
+          source="mealId"
+          label="Meal"
+          validate={[required()]}
+          choices={ mealChoices }
         />
-      </ReferenceInput>
-      <ReferenceInput source="mealId" reference="meals">
-        <SelectInput optionText="nameEn" label="Meal" validate={[required()]} />
-      </ReferenceInput>
     </SimpleForm>
   );
 };
