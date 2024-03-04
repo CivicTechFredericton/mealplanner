cfb76a101e6106c4a6d9be7bbb277673ffc32ab1
Priya <guntupallisaipriya@gmail.com>
2024-03-04 16:59:14 -0400

    filter meals based on tags using relay

--- mealplanner-ui/src/pages/Meals/Meals.tsx
+++ mealplanner-ui/src/pages/Meals/Meals.tsx
@@ -1,12 +1,15 @@
-import { Search } from "@mui/icons-material";
 import {
+  FormControl,
+  FormControlLabel,
   Grid,
   InputBase,
-  Paper
+  Radio,
+  RadioGroup
 } from "@mui/material";
 import { graphql } from "babel-plugin-relay/macro";
-import { useState } from "react";
-import { useLazyLoadQuery } from "react-relay";
+import {Suspense, useState } from "react";
+import { useLazyLoadQuery, useRefetchableFragment } from "react-relay";
+import { MealTags, MealTagsFragment } from "./MealTags";
 import { MealsQuery } from "./__generated__/MealsQuery.graphql";
 import { MealCard } from "./MealCard";
 
@@ -26,59 +29,101 @@ const mealsQuery = graphql`
         videoUrl
       }
     }
+    # fragment name from MealTags
+    ...MealTags_tags
+    gqLocalState {
+      selectedMealTags
+    }
   }
 `;
 
-
-
 export const Meals = () => {
   const [searchMeal, setSearchMeal] = useState<string>("");
+  const [searchType, setSearchType] = useState('name');
+
   const data = useLazyLoadQuery<MealsQuery>(
     mealsQuery,
     {},
     { fetchPolicy: "store-or-network" }
   );
+
+  const selectedTags = data.gqLocalState.selectedMealTags || [];
+
   return (
+    <div>
     <Grid
       container
       spacing={2}
       columns={2}
-      justifyContent="center"
-      marginTop="1rem"
+      gap="2rem"
+      margin="1rem 2rem"
+      width="95%"
+      justifyContent="space-between"
     >
-      <Paper
-        component="form"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          width: "75%",
-          justifyContent: "center",
+      <FormControl component="fieldset">
+      <RadioGroup
+        row
+        aria-label="searchType"
+        name="searchType"
+        value={searchType}
+        onChange={(e) => {
+          setSearchType(e.target.value);
         }}
       >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search Meal"
-          inputProps={{ "aria-label": "Search Meal" }}
-          onChange={(e) => setSearchMeal(e.target.value.toLowerCase())}
+       <FormControlLabel
+          value="name"
+          control={
+            <Radio 
+              checked={searchType === 'name'}
+            />
+          }
+          label={
+            <InputBase
+              placeholder="Search Meal"
+              inputProps={{ "aria-label": "Search Meal" }}
+              readOnly={searchType !== 'name'}
+              value={searchMeal}
+              onChange={(e) => {
+                if (searchType === 'name') {
+                  setSearchMeal(e.target.value.toLowerCase());
+                }
+              }}
+              style={{ cursor: 'text',
+                      borderBottom: '1px solid black', width: '40vw' }}
+              />
+          }
         />
-        <Search />
-      </Paper>
-      {data.meals ? (
-        <Grid
-          container
-          spacing={2}
-          justifyContent="center"
-          marginTop="1rem"
-          columns={4}
-        >
-          {data.meals?.nodes.map((node) => {
-            if (node.nameEn.toLowerCase().includes(searchMeal))
-              return <MealCard node={node} />;
-          })}
-        </Grid>
+        <FormControlLabel
+          value="favorites"
+          control={<Radio />}
+          label="Favorites"
+          checked={searchType === 'favorites'}
+        />
+        <FormControlLabel
+          value="tags"
+          control={<Radio />}
+          label="Tags"
+          checked={searchType === 'tags'}
+        /> 
+      </RadioGroup>
+    </FormControl>
+    </Grid>
+      {searchType === 'tags' &&
+        <Suspense fallback={"loading tags..."}>
+        <MealTags tags={data}/>
+      </Suspense>
+    }
+   
+    {data.meals ? (
+      <Grid container spacing={2} margin="1rem" columns={4}>
+        {data.meals?.nodes.map((node) => {
+      if ((searchType === 'name' && node.nameEn.toLowerCase().includes(searchMeal)) || (searchType === 'tags' && selectedTags.every(tag => node.tags?.includes(tag)))) {
+        return <MealCard node={node} />
+    }})}
+  </Grid>
       ) : (
         "No meals"
       )}
-    </Grid>
+    </div>
   );
 };
