34173030542d1be7faad90eda7cb770b3926576d
okayyjen <bkim3@unb.ca>
2024-03-11 14:08:23 -0300

    [#460] - adding delete confirmation dialogue. Name of meal plan is now shown in confirmation dialog

--- mealplanner-ui/src/pages/MealPlans/MealPlanCard.tsx
+++ mealplanner-ui/src/pages/MealPlans/MealPlanCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MealPlanNode } from "../../state/types";
-import { Avatar, Card, CardActions, CardContent, CardHeader, Collapse, Grid, IconButton, IconButtonProps, ImageList, ImageListItem, Typography, styled } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, Collapse, Grid, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, IconButtonProps, ImageList, ImageListItem, Typography, styled, useMediaQuery, useTheme } from "@mui/material";
 import { ShoppingCart, DeleteTwoTone, ContentCopy, ExpandMore, Favorite } from "@mui/icons-material";
 import { useNavigate } from "react-router";
 import { getCurrentPerson } from "../../state/state";
@@ -45,6 +45,9 @@ const getInitials = (name: string) => {
 
 export const MealPlanCard = (props: MealPlanCardProps) => {
     const [expanded, setExpanded] = React.useState(false);
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const theme = useTheme();
+    const fullScreenDialog = useMediaQuery(theme.breakpoints.down('md'));
     const navigate = useNavigate();
     const mealplan = props.mealplan;
     const connection = props.connection;
@@ -52,6 +55,25 @@ export const MealPlanCard = (props: MealPlanCardProps) => {
       e.stopPropagation();
       setExpanded(!expanded);
     };
+    const handleClickOpen = (e: React.MouseEvent) => {
+      e.stopPropagation();
+      setOpenDialog(true);
+    };
+  
+    const handleClose = (e: React.MouseEvent) => {
+      e.stopPropagation();
+      setOpenDialog(false);
+    };
+  
+    const handleDelete =(e: React.MouseEvent) => {
+      e.stopPropagation();
+      deleteMealPlan(connection, mealplan.rowId).then(() => {
+        //refetch here for fetching tags after delete
+        props.refetch({}, {fetchPolicy: "network-only"})
+      })
+      setOpenDialog(false);
+    };
+  
   
     return (
       <Grid item xs="auto">
@@ -85,18 +107,44 @@ export const MealPlanCard = (props: MealPlanCardProps) => {
                   <ShoppingCart />
                 </IconButton>
                 <IconButton
-                  aria-label="delete"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    console.log("meal plan id: ", typeof mealplan.rowId);
-                    deleteMealPlan(connection, mealplan.rowId).then(() => {
-                      //refetch here for fetching tags after delete
-                      props.refetch({}, {fetchPolicy: "network-only"})
-                    })
-                  }}
-                >
-                  <DeleteTwoTone />
-                </IconButton>
+                aria-label="delete"
+                onClick = {handleClickOpen}
+              >
+                <DeleteTwoTone />
+              </IconButton>
+              <Dialog
+                fullScreen={fullScreenDialog}
+                open={openDialog}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }}
+                onClose={handleClose}
+                aria-labelledby="delete-dialog"
+              >
+                <DialogTitle id="delete-dialog">
+                  {"Delete this meal plan?"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Are you sure you want to <b>delete</b> the meal plan <b>{mealplan.nameEn}</b>?
+                  </DialogContentText>
+                </DialogContent>
+                    
+                <DialogActions>
+                  <Button 
+                    onClick={handleDelete}
+                    autoFocus
+                    startIcon={<DeleteTwoTone />}
+                    color = "error"
+                  >
+                    Delete
+                  </Button>
+                  <Button autoFocus onClick={handleClose}>
+                    Cancel
+                  </Button>
+                </DialogActions>
+              </Dialog>
                 {getCurrentPerson().personRole === "app_admin" || getCurrentPerson().personRole === "app_meal_designer" ? (
                   <IconButton
                     aria-label="duplicate"
