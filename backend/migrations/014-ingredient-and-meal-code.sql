095844bbef454c60029bfb4db78da6501a64e575
Shanthi Shanmugam <shanthi.shanmugam@gmail.com>
2024-03-20 10:35:16 -0300

    [[FEATURE] [#564]  Making the ingredient and meal code unique

--- backend/migrations/014-ingredient-and-meal-code.sql
+++ backend/migrations/014-ingredient-and-meal-code.sql
@@ -1,6 +1,6 @@
 begin;
-ALTER table app.meal add column if not exists code int;
-ALTER table app.ingredient add column if not exists code int;
+ALTER table app.meal add column if not exists code int unique;
+ALTER table app.ingredient add column if not exists code int unique;
 comment on column app.meal.code is 'Unique code for the meal such 1, 2, 3, etc to later show as M001 - need not be same as the db id';
 comment on column app.ingredient.code is 'Unique code for the ingredient such 1, 2, 3, to later combine as M001-I01';
 commit;
